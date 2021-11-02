1af4f0fe23fad3686e536e23b34a797b4ac0efa3
rodsang <angelica.rodrigues@tlantic.com>
2021-11-02 15:37:00 +0000

    msg

--- App.js
+++ App.js
@@ -1,15 +1,62 @@
+import 'react-native-gesture-handler';
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Homepage from './Home';
+import { Platform } from 'react-native';
+import { useFonts } from 'expo-font';
+import {AppLoading} from 'expo';
+import Header from './Header';
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  let [fontsLoaded]=useFonts({
+
+    'OpenSans': require('./assets/fonts/OpenSans-Regular.ttf'),
+  });
+  
+
+  if (!fontsLoaded){
+    return <AppLoading/>;
+  } else {
+    return (
+      <NavigationContainer>
+        style={{paddingTop:Platform.OS=='android' ? StatusBar.currentHeight:0}}
+      <Stack.Navigator
+      initialRouteName='Globomantics'
+      headerMode="screen">
+        <Stack.Screen name='Globomantics' 
+        component={Homepage}
+        options={{
+          header:()=> <Header headerDisplay="Globomantics"></Header>
+        }}>
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+    );
+  }
+  }
+
+
+  /*
+    )
+    
+  }
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Globomantics">
+        <Stack.Screen name="Globomantics" component={Homepage}>
+
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +66,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+*/
